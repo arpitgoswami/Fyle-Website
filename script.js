897ab9a7e0f76f81197a8b5ee83044c0c8423d37
Arpit Goswami <arpitgoswami@hotmail.com>
2024-06-02 12:39:08 +0530

    No Carousel

--- script.js
+++ script.js
@@ -7,11 +7,69 @@ contact.addEventListener("mousedown", () => {
   blur.style.display = "block";
 });
 
-const close = () => {
+const closeForm = () => {
   form.style.display = "none";
   blur.style.display = "none";
 };
 
 const redirect = () => {
-  window.location("https://fylehq.com", "_blank");
+  window.open("https://fylehq.com", "_blank");
 };
+
+const box1 = document.querySelectorAll("#box1");
+const box2 = document.getElementById("box2");
+const image = document.getElementById("project-img");
+
+box1[0].addEventListener("mouseover", () => {
+  image.src = "./assets/image.png";
+});
+
+box2.addEventListener("mouseover", () => {
+  image.src = "./assets/image2.jpg";
+});
+
+box1[1].addEventListener("mouseover", () => {
+  image.src = "./assets/image3.jpg";
+});
+
+// carousel scripting
+
+let slideIndex = 0;
+
+function showSlide(n) {
+  const slides = document.querySelectorAll(".carousel-item");
+  const dots = document.querySelectorAll(".carousel-dot");
+  if (n >= slides.length) {
+    slideIndex = 0;
+  }
+  if (n < 0) {
+    slideIndex = slides.length - 1;
+  }
+  slides.forEach((slide, index) => {
+    slide.style.transform = `translateX(${-slideIndex * 100}%)`;
+    if (index === slideIndex) {
+      slide.classList.add("active");
+    } else {
+      slide.classList.remove("active");
+    }
+  });
+  dots.forEach((dot, index) => {
+    if (index === slideIndex) {
+      dot.classList.add("active");
+    } else {
+      dot.classList.remove("active");
+    }
+  });
+}
+
+function nextSlide() {
+  showSlide(++slideIndex);
+}
+
+function prevSlide() {
+  showSlide(--slideIndex);
+}
+
+function currentSlide(n) {
+  showSlide((slideIndex = n));
+}
